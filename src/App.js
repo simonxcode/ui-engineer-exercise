f1e0ad3073cdff900fd6c4819da3cfbac3f1e9d0
simonxcode <simonxiong11@gmail.com>
2022-02-21 10:56:53 -0500

    Initial styling and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { createGlobalStyle } from 'styled-components'
 import People from './components/people'
 
+const GlobalStyle = createGlobalStyle `
+  body {
+    font-family: Proxima Nova, Arial, Helvetica, sans-serif;
+    background-color: #F9FAFB;
+  }
+`
 export const App = () => {
-  return <><People /></>;
+  return (
+    <>
+      <GlobalStyle />
+      <People />
+    </>
+  )
 };
--- src/components/people/people.jsx
+++ src/components/people/people.jsx
@@ -1,15 +1,68 @@
 import React from "react";
+import styled from 'styled-components'
 import ProfilePanel from './profile-panel'
 import Activity from './activity-panel'
+
+const Header = styled.div`
+  display: block;
+  background-color: #FFFFFF;
+  border-bottom: 1px solid #E5E5E5; 
+`
+const Content = styled.div`
+  display: flex;
+  padding: .5rem;
+`
+
+const LeftColumn = styled.div`
+  width: 25%
+`
+
+const CenterColumn = styled.div`
+  width: 50%
+`
+
+const RightColumn = styled.div`
+  width: 25%;
+`
+
+const Panel = styled.div`
+  margin: .5rem;
+  background-color: #FFFFFF;
+  border: 1px solid #E5E5E5; 
+`
+
+const BlankPanel = styled(Panel)`
+  height: 166px;
+`
 
 const People = () => {
   return (
     <>
-      <p>People Header</p>
-      <ProfilePanel />  
-      <Activity />  
-    </>    
+     <Header>People</Header>
+    <Content>
+      <LeftColumn>
+        <Panel>
+          <ProfilePanel /> 
+        </Panel>
+        <BlankPanel />
+        <BlankPanel />
+        <BlankPanel />
+      </LeftColumn>
+      <CenterColumn>
+        <Panel>
+          <Activity />  
+        </Panel>
+      </CenterColumn>
+      <RightColumn>
+        <BlankPanel />
+        <BlankPanel />
+        <BlankPanel />
+        <BlankPanel />
+      </RightColumn>
+    </Content>    
+    </>
   )
 }
 
 export default People
+
