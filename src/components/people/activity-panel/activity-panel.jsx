df7bc7ef2f7364b7dbf5d154732511bb5c7a839f
simonxcode <simonxiong11@gmail.com>
2022-02-21 17:09:01 -0500

    Format date and time

--- src/components/people/activity-panel/activity-panel.jsx
+++ src/components/people/activity-panel/activity-panel.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
 const ActivityPanel = ({activity}) => {
 
   const activityType = activity.type
   const user = activity.dynamic_data.user_name
-  const createDate = activity.created_at
-  const createTime = activity.create_at
+  const createDate = moment.utc(activity.created_at).format('MMM D, YYYY')
+  const createTime = moment.utc(activity.occurred_at).format('hh:mm A')
 
   return (
     <>
